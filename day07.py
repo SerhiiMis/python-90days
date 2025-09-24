43ec4129a17aa8af4eed80bac6bef451a75179c9
Serhii Mishovych <serhiimishovych@gmail.com>
2025-09-24 20:27:38 +0200

    day07: review quiz for Days 1–6

---
+++ day07.py
@@ -0,0 +1,18 @@
+a = "John"
+b = 30
+c = "a software developer"
+
+print(f"My name is {a}, I am years {b} old, and I work as {c}.")
+
+
+x = 12
+y = 3.14
+
+print(x + y, x - y, x * y, x / y, x // y, x % y, x ** 2)
+
+
+message = "Industrial Python"
+
+print(message.upper(), message[0:3], message[-3:])
+
+machines = ["Netstal", "Arburg", "KraussMaffei", "Haitian", "Engel", "Sumitomo"]
