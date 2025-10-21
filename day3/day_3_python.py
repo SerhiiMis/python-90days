2a9bf634e2b72a5f87aef9b977893beeb8516737
Serhii Mishovych <serhiimishovych@gmail.com>
2025-10-21 20:04:59 +0200

    Added function with multiple inputs

--- day3/day_3_python.py
+++ day3/day_3_python.py
@@ -7,6 +7,7 @@ component_data = {
     "is_critical": True
 }
 
+
 part_name = component_data["name"]
 print(f"Part being inspected: {part_name}")
 
@@ -20,6 +21,7 @@ print("\n--- Component Details ---")
 for key, value in component_data.items():
     print(f"{key.replace('_', ' ').title()}: {value}")
 
+
 def get_station_label(station_number):
     """Returns the descriptive name for an assembly station number."""
     if station_number == 1:
@@ -32,3 +34,27 @@ def get_station_label(station_number):
 station = component_data["assembly_station"]
 label = get_station_label(station)
 print(f"\nPart is at station {station}, which is the: {label}")
+
+
+daily_inspection_log = [
+    {"id": 1, "status": "PASS", "temp": 21.5},
+    {"id": 2, "status": "FAIL", "temp": 28.1},
+    {"id": 3, "status": "PASS", "temp": 22.9},
+    {"id": 4, "status": "FAIL", "temp": 25.5},
+    {"id": 5, "status": "PASS", "temp": 23.0},
+]
+
+
+def calculate_efficiency(actual_time, target_time):
+    """Calculates and returns the process efficiency score."""
+    efficiency = (target_time / actual_time) * 100
+    return round(efficiency, 1) 
+
+actual = component_data["cycle_time_sec"]
+target = 40.0 # Target time for a perfect cycle
+
+efficiency_score = calculate_efficiency(actual, target)
+print(f"Actual time: {actual}s. Target time: {target}s.")
+print(f"Process Efficiency: {efficiency_score}%")
+
+
